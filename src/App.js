f088f0ee87e20c665be6c982511b6da771fa2eeb
Jeliel Braz <jeliel.braz@neomind.com.br>
2023-09-19 22:16:01 -0300

    WIP: start index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render () {
     return (
-    <div className="App">
-      <header className="App-header">
+      <div className="App">
+        <header className="App-header">
+          { this.getRandomWelcomeMessage() }
 
+          <div class="container text-center">
+            <div class="row justify-content-md-center">
+              <div class="col-6">
+                <button type="button" class="btn btn-secondary">Login</button>
+              </div>
+              <div class="col-6">
+                <button type="button" class="btn btn-secondary">Criar uma conta</button>
+              </div>
+            </div>
+          </div>
 
-      </header>
-    </div>
-  );
+          
+          
+  
+        </header>
+      </div>
+    );
+  }
+
+  getRandomWelcomeMessage() {
+    let welcomeMessages = [
+      "MAX MAX SUPER MAX",
+      "I'm supid",
+      "It's Ericsson's fault"
+    ];
+
+    return welcomeMessages[Math.floor(Math.random()*welcomeMessages.length)];   
+  }
 }
 
 export default App;
